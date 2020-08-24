35ea4fc465ee781c95df8cda2b963b2f3d778154
aaron <aaronzstv@gmail.com>
2020-08-24 15:24:58 +1000

    win screen added

--- Assets/Scripts/EnemiesLeftCounter.cs
+++ Assets/Scripts/EnemiesLeftCounter.cs
@@ -5,17 +5,25 @@ public class EnemiesLeftCounter : MonoBehaviour
 {
     private Text counterText;
     private float enemyCounter;
+    public Canvas completeCanvas;
+    public Canvas gamePlayScreen;
     // Start is called before the first frame update
     void Start()
     {
+        completeCanvas.enabled = false;
         counterText = GetComponent<Text>();
-        //getNumberOfEnemies();
     }
 
     public void subtract()
     {
         enemyCounter--;
         counterText.text = "Enemies left: " + enemyCounter;
+        if(enemyCounter <= 0)
+        {
+            completeCanvas.enabled = true;
+            gamePlayScreen.enabled = false;
+            Time.timeScale = 0.0f;
+        }
     }
 
     public void add()
