8c41a1130c772c7fa3b98423d994b12c4015ae22
Nixon Sok <nixon.s.aie@outlook.com>
2020-09-01 12:34:49 +1000

    Added Level Transition

---
+++ Assets/LevelTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    LevelLoader levelLoader;
+
+    private void Awake()
+    {
+        levelLoader = GameObject.Find("LevelChanger").GetComponent<LevelLoader>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+
+        if (other.layer == 8)
+        {
+            levelLoader.LoadLevelNext();
+        }
+    }
+}
