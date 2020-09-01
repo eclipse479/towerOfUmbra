f62d7215c6a7ac8324fc95bb445b388bc73a2713
Adam <adamjhouse5@gmail.com>
2020-09-01 14:53:06 +1000

    Added prefabs and scripts, tweaked scenes and prefabs

---
+++ Assets/PrototypeGMTut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PrototypeGMTut : MonoBehaviour
+{
+    public KeyCode restartKey = KeyCode.R;
+
+
+    public KeyCode QuitKey = KeyCode.Q;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(restartKey))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown(QuitKey))
+        {
+            Application.Quit();
+            Debug.Log("Bye");
+        }
+    }
+}
