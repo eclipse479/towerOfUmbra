91ec596cda6461906b4e6e5c0e8d6fc0ff70d49d
Nixon Sok <nixon.s.aie@outlook.com>
2020-10-26 16:17:15 +1100

    Skeleton won't explode during death and stun.
    Stop setting detect_distance to being less than attack range!

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -67,7 +67,6 @@ public class EnemyBehaviour : MonoBehaviour
     [Tooltip("Either the root node or whatever object is called the ray centre")] public Transform ray_centre;
 
     // Keep track of the player
-    [Tooltip("How far the enemy can see the player")] public float detect_distance = 5.0f;
     float distance_to_player;
 
     // Rays to check on each side
@@ -484,7 +483,7 @@ public class EnemyBehaviour : MonoBehaviour
         // Check where the player is
         distance_to_player = (target.position - transform.position).magnitude;
 
-        if (distance_to_player < detect_distance)
+        if (distance_to_player < detection_range)
         {
             transform.forward = new Vector3((target.position - transform.position).x, 0, 0).normalized;
             behaviour = STATE.CHASING;
@@ -581,6 +580,7 @@ public class EnemyBehaviour : MonoBehaviour
     void die()
     {
         textCounter.subtract();
+        FindObjectOfType<SoundManager>().playSound("skeletonDies");
         gameObject.tag = "Untagged";
         gameObject.layer = 4;
         healthBar.gameObject.SetActive(false);
@@ -632,6 +632,8 @@ public class EnemyBehaviour : MonoBehaviour
         { 
             is_stunned = true;
 
+            FindObjectOfType<SoundManager>().playSound("skeletonHit");
+
             // Reset Enemy velocity
             rb.velocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
@@ -652,6 +654,8 @@ public class EnemyBehaviour : MonoBehaviour
             animator.SetTrigger("Death");
         }
     }
+
+ 
 
     public bool IsDead
     {
