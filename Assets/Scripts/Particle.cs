0e40ab0a22f0c4df2017ca228a0585afed77c430
Nixon Sok <nixon.s.aie@outlook.com>
2020-11-01 10:41:51 +1100

    Added ParticleManager
    You can add particles to the manager and call it in any script.
    
    Deficiencies: Makes the game stall at times.

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Particle 
+{
+    [Tooltip("Name to be referenced")] public string name;
+
+    [Tooltip("Actual Particle")] public ParticleSystem particle;
+
+    [Tooltip("How quickly the particles emit")] public float emission_rate = 1.0f;
+
+    [Tooltip("Size of the particle emissions")] public float emission_size = 1.0f;
+
+    [Tooltip("How long the particle plays for")] public float duration = 1.0f;
+
+    [Tooltip("Does the particle loop?")] public bool loop = false;
+
+    [Tooltip("Destroy when done?")] public ParticleSystemStopAction particle_end;
+}
